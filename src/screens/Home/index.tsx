925de59bbb31fd0fbda13e7082f50549802deef2
yavitaliks <yavitaliks@gmail.com>
2022-01-16 10:21:47 -0300

    Dia-01

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+export function Home(){
+    return(
+        <View>
+            <Text> Teste de aplicação </Text>;
+        </View>
+    )
+}
